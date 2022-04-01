a8d8e30a13a3a1ddefdced6dd8c569b25116024d
KiefeEd <kiefe.edward@gmail.com>
2022-04-01 20:30:10 +0700

    changed print style

--- aritmatika.go
+++ aritmatika.go
@@ -3,13 +3,13 @@ package goaritmatika
 import "fmt"
 
 func Penjumlahan(x,y int16) {
-	fmt.Println("Hasil dari penjumlahan dari %d + %d adalah", x + y)
+	fmt.Printf("Hasil dari penjumlahan dari %d + %d adalah", x + y)
 }
 
 func Perkalian(x,y int16) {
-	fmt.Println("Hasil dari perkalian %d * %d adalah", x*y)
+	fmt.Printf("Hasil dari perkalian %d * %d adalah", x*y)
 }
 
 func Pembagian(x,y int16) {
-	fmt.Println("Hasil dari pembagian %d * %d adalah", x/y)
+	fmt.Printf("Hasil dari pembagian %d * %d adalah", x/y)
 }
