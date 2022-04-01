cd748609ddddcf79c7168f50041f0589332b3751
KiefeEd <kiefe.edward@gmail.com>
2022-04-01 20:14:24 +0700

    More detailed display for penjumlahan

--- aritmatika.go
+++ aritmatika.go
@@ -3,6 +3,5 @@ package goaritmatika
 import "fmt"
 
 func Penjumlahan(x,y int16) {
-	hasil := x + y
-	fmt.Println("Hasil dari penjumlahan tersebut adalah", hasil)
+	fmt.Println("Hasil dari penjumlahan dari %d + %d adalah", x + y)
 }
